8a67a29f3a8d0ff329c2ee11da2e148edeb43e28
Mirageinvo <70330507+Mirageinvo@users.noreply.github.com>
2020-08-27 23:42:16 +0300

    Update Sq_equat.cpp

--- Sq_equat.cpp
+++ Sq_equat.cpp
@@ -3,54 +3,70 @@
 #include <stdio.h>
 #include <math.h>
 
-int check(double a, double b, double c) {
+enum cases { infinite, no_answer, linear, regular };
+enum cases_discriminant { no_roots, one_root, two_roots };
+
+int check_of_coeff_in_quadr_equat(double a, double b, double c) {
     if (a == 0 && b == 0 && c == 0)
-        return 0;
+        return infinite;
     else if (a == 0 && b == 0)
-        return 1;
+        return no_answer;
     else if (a == 0)
-        return 2;
+        return linear;
     else {
-        return 3;
+        return regular;
+    }
+}
+
+int quadratic_solver(double a, double b, double c, double& x1, double& x2) {
+    double disc = b * b - 4 * a * c;
+    if (disc < 0)
+        return no_roots;
+    else if (disc < 1e-6) {
+        x1 = (-1.0) * b / 2 / a;
+        return one_root;
+    }
+    else {
+        disc = sqrt(disc);
+        x1 = ((-1.0) * b - disc) / 2 / a;
+        x2 = ((-1.0) * b + disc) / 2 / a;
+        return two_roots;
+    }
+}
+
+void writing_ans(double a, double b, double c, double& x1, double& x2) {
+    switch (check_of_coeff_in_quadr_equat(a, b, c)) {
+        case infinite:
+            printf("Infinite number of roots\n");
+            break;
+        case no_answer:
+            printf("No roots\n");
+            break;
+        case linear:
+            x1 = (-1.0) * c / b;
+            printf("This is not a quadratic equation but the answer is: %lg\n", x1);
+            break;
+        case regular:
+            switch (quadratic_solver(a, b, c, x1, x2)) {
+                case no_roots:
+                    printf("No roots\n");
+                    break;
+                case one_root:
+                    printf("X = %lg\n", x1);
+                    break;
+                case two_roots:
+                    printf("X_1 = %lg\nX_2 = %lg\n", x1, x2);
+                    break;
+                }
+            break;
     }
 }
 
 int main() {
-    float a = 0, b = 0, c = 0;
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
     printf("# Solving of equations\n# Input a, b, c: ");
-    scanf("%g %g %g", &a, &b, &c);
+    scanf("%lg %lg %lg", &a, &b, &c);
     printf("\n");
-    switch (check(a, b, c)) {
-        case 0: {
-            printf("Infinite number of roots\n");
-            break;
-        }
-        case 1: {
-            printf("No roots\n");
-            break;
-        }
-        case 2: {
-            float x = (-1.0) * c / b;
-            printf("This is not a quadratic equation but the answer is: %lg\n", x);
-            break;
-        }
-        case 3: {
-            float disc = b * b - 4 * a * c;
-            if (disc < 0) {
-                printf("No roots\n");
-            }
-            else if (disc < 1e-6) {
-                float x = (-1.0) * b / 2 / a;
-                printf("X = %g\n", x);
-            }
-            else {
-                float sqrt_disc = sqrt(disc);
-                float x1 = ((-1.0) * b + sqrt_disc) / 2 / a;
-                float x2 = ((-1.0) * b - sqrt_disc) / 2 / a;
-                printf("X_1 = %g\nX_2 = %g\n", x1, x2);
-            }
-            break;
-        }
-    }
+    writing_ans(a, b, c, x1, x2);
     return 0;
 }
